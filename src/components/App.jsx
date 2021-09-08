af91151ad001ea4c8f282e6bee6ef90b25527b0d
Djaypm89 <djaypm89@gmail.com>
2021-09-08 12:15:07 -0400

    catchup push

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import './App.css'
 import TitleBar from "./TitleBar/TitleBar";
+import BookViewer from "./BookViewer/BookViewer";
+
 
 class App extends Component {
     constructor(props) {
@@ -15,22 +17,33 @@ class App extends Component {
         };
     }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        })
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
     render() {
         return (
            <div className="container-fluid">
                <TitleBar />
-               <div className="col-md-4">
-                    {/*Button here to move to previous book viewed*/}
-                </div>
-                <div className="col-md-4">
-                    {/*Display book with cover here*/}
-                    <h1>{this.books[this.state.bookNumber].title}</h1>
-                    <h4>{this.books[this.state.bookNumber].author}</h4>
-                </div>
-                <div className="col-md-4">
-                    {/*Button here to move to next book viewed*/}
-               </div>
-
+               <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
            </div>
         )
     }
