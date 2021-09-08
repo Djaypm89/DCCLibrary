0cebe73407cca9220dff257c519ca5b7ba4e8c82
Djaypm89 <djaypm89@gmail.com>
2021-09-07 23:49:34 -0400

    vid 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,32 @@ import TitleBar from "./TitleBar/TitleBar";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "The Alchemist", author: "Paulo Coelho"},
+            {title: "The Travels of Marco Polo", author: "Marco Polo"},
+            {title: "Siddhartha", author: "Herman Hesse"}
+        ];
+        this.state = {
+            bookNumber: 1
+        };
     }
 
-    render(){
+    render() {
         return (
            <div className="container-fluid">
                <TitleBar />
+               <div className="col-md-4">
+                    {/*Button here to move to previous book viewed*/}
+                </div>
+                <div className="col-md-4">
+                    {/*Display book with cover here*/}
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                </div>
+                <div className="col-md-4">
+                    {/*Button here to move to next book viewed*/}
+               </div>
+
            </div>
         )
     }
